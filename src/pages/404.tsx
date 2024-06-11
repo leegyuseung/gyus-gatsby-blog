d5790ab233cbf532ddb56280e3cc31d7e5da9e4b
lee gyuseung <gyus0421@gmail.com>
2024-06-11 10:01:36 +0900

    feat : 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,46 @@
+import styled from '@emotion/styled'
+import GlobalStyle from 'components/Common/GlobalStyle'
+import Logo from 'components/Common/Logo'
+import { Link } from 'gatsby'
+import React, { FunctionComponent } from 'react'
+
+const NotFoundPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const NotFoundText = styled.div`
+  font-size: 150px;
+  font-weight: 800;
+`
+
+const NotFoundDescription = styled.div`
+  font-size: 25px;
+  text-align: center;
+  line-height: 1.3;
+`
+
+const LogoButtonWrapper = styled.div`
+  margin-top: 25px;
+`
+
+const NotFoundPage: FunctionComponent = () => {
+  return (
+    <NotFoundPageWrapper>
+      <GlobalStyle />
+      <NotFoundText>404</NotFoundText>
+      <NotFoundDescription>
+        찾을 수 없는 페이지입니다. <br />
+        메인 페이지로 이동하시겠습니까?
+      </NotFoundDescription>
+      <LogoButtonWrapper>
+        <Logo width={'40px'} height={'40px'} radius={'0px'} />
+      </LogoButtonWrapper>
+    </NotFoundPageWrapper>
+  )
+}
+
+export default NotFoundPage
