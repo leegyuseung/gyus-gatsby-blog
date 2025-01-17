a3644b7b14360f7d3f902eb1c056480629d38bf1
leegyuseung <gyus0421@gmail.com>
2025-01-17 16:15:32 +0900

    feat : diary
    diary 페이지 생성

---
+++ src/layout/DiaryListLayout.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import ListItem from 'components/Item/ListItem'
+import { ListLayoutProps } from 'types/ListLayout.types'
+
+const DiaryContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 100%;
+  padding: 20px 0;
+`
+
+const DiaryListLayout = ({ posts }: ListLayoutProps) => {
+  return (
+    <DiaryContainer>
+      {posts.map((post, index) => {
+        const imageData =
+          post.node.frontmatter.thumbnail?.childImageSharp?.gatsbyImageData
+
+        return <ListItem imageData={imageData} key={index} post={post} />
+      })}
+    </DiaryContainer>
+  )
+}
+
+export default DiaryListLayout
--- src/pages/diary/index.tsx
+++ src/pages/diary/index.tsx
@@ -1,11 +1,45 @@
-import BaseLayout from '../../layout/BaseLayout'
+import { graphql } from 'gatsby'
+import { DiaryPageProps } from 'types/DiaryPage.types'
 
-const DiaryPage = () => {
+import BaseLayout from '../../layout/BaseLayout'
+import DiaryListLayout from '../../layout/DiaryListLayout'
+
+const DiaryPage = ({ data }: DiaryPageProps) => {
   return (
     <BaseLayout>
-      <p>DiaryPage</p>
+      <DiaryListLayout posts={data.allMdx.edges} />
     </BaseLayout>
   )
 }
+
+export const pageQuery = graphql`
+  query {
+    allMdx(
+      filter: { frontmatter: { categories: { in: ["diary"] } } }
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date
+            tags
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData(
+                  width: 200
+                  height: 150
+                  formats: [AUTO, WEBP, AVIF]
+                )
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default DiaryPage
