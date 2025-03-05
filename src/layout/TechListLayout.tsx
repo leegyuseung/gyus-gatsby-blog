ff2b523928bcbd93aaa23f287bdf5e5c4f7b0966
leegyuseung <gyus0421@gmail.com>
2025-03-05 17:51:18 +0900

    style : tech, diary

--- src/layout/TechListLayout.tsx
+++ src/layout/TechListLayout.tsx
@@ -10,15 +10,40 @@ const ListContainer = styled.div`
   width: 100%;
   padding: 20px 0;
 `
+const SectionTitle = styled.h2`
+  font-size: 30px;
+  text-align: start;
+  color: #262a2d;
+  display: inline-block; /* 텍스트 길이에 맞춰 border 생성 */
+`
+const TitleDivider = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #262a2d;
+`
+
+const Divider = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #919497;
+  margin: 5px 0;
+`
 
 const ListLayout = ({ posts }: ListLayoutProps) => {
   return (
     <ListContainer>
+      <SectionTitle># Tech</SectionTitle>
+      <TitleDivider />
       {posts.edges.map((post, index) => {
         const imageData =
           post.node.frontmatter.thumbnail?.childImageSharp?.gatsbyImageData
 
-        return <ListItem imageData={imageData} key={index} post={post.node} />
+        return (
+          <>
+            <ListItem imageData={imageData} key={index} post={post.node} />
+            <Divider />
+          </>
+        )
       })}
     </ListContainer>
   )
